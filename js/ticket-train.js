0fb952b242230cadbaaad943257ab00b9132c495
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-14 15:50:09 +0200

    Add description and user km

---
+++ js/ticket-train.js
@@ -0,0 +1,27 @@
+// # TRACCIA
+
+//* Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+
+//* e sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+
+//* - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+//* - va applicato uno sconto del 20% per i minorenni
+
+//* - va applicato uno sconto del 40% per gli over 65.
+
+//* - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
+
+// # PROCEDURA
+
+// ! RACCOLTA DATI
+
+//* chiedo all'utente il numero di chlometri che vuole percorrrere
+const numberKm = parseInt(
+  prompt("Inserisci il numero di chilometri che devi percorrere")
+);
+console.log(numberKm);
+
+// ! ELABORAZIONE
+
+// ! OUTPUT
