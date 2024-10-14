87a35181861422843b7ede3c2258052b2fdabeac
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-14 18:51:11 +0200

    Add discount ticket for under 18 and over 65

--- js/ticket-train.js
+++ js/ticket-train.js
@@ -22,6 +22,9 @@ const numberKm = parseInt(
 );
 console.log(numberKm);
 
+const kmCost = 0.21;
+console.log(kmCost);
+
 //* controllo che sia valido
 const isNumberKmValid = !isNaN(numberKm) && numberKm > 0;
 
@@ -40,35 +43,32 @@ if (!isPassengerAgeValid) {
   alert("Il valore inserito non è valido");
 }
 
+//* sconto del 20% da applicare per gli under 18
+
 // ! ELABORAZIONE
 
 //* calcolo prezzo biglietto (0.21 € al km)
-const kmCost = 0.21;
-console.log(kmCost);
 
 const priceTicket = numberKm * kmCost;
 console.log(priceTicket);
 
-//** sconto del 20% per gli under 18
-const under18Discount = 20;
-const priceForUnder18 = priceTicket - under18Discount;
-
-if (passengerAgeDiscount) { 
-    /* se il passeggero ha meno di 18 anni */
-    passengerAge < 18;
-  alert("Hai meno di 18 anni quindi viene applicato uno sconto del 20%");
-  
-  else {
-
-  }
-
+if (passengerAge < 18) {
+  /* se il passeggero ha meno di 18 anni */
+  const discountForUnder18 = 20;
+  const priceUnder18 = priceTicket - discountForUnder18;
+  console.log(priceUnder18.toFixed(2));
+  alert(
+    `Hai meno di 18 anni quindi viene applicato uno sconto del 20%, il prezzo è di ${priceUnder18}`
+  );
 }
 
-//** sconto del 40% per gli over 65
-const over65Discount = 40;
-const priceForOver65 = priceTicket - over65Discount;
-if (over65Discount) {
-  alert("Per chi ha più di 65 anni viene applicato uno sconto del 40%");
+if (passengerAge > 65) {
+  /* se il passeggero ha più di 65 anni*/
+  const discountForOver65 = 40;
+  const priceOver65 = priceTicket - discountForOver65;
+  console.log(priceOver65.toFixed(2));
+  alert(
+    `Hai più di 65 anni quindi viene applicato uno sconto del 40%, il prezzo è di ${priceOver65}`
+  );
 }
-
 // ! OUTPUT
