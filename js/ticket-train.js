89182eb5e215d7013f7f7cfc4b44ec66e08bacb5
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-20 14:06:26 +0200

    Add fix output message ticket

--- js/ticket-train.js
+++ js/ticket-train.js
@@ -12,64 +12,61 @@
 
 //* - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
 
-// # PROCEDURA
-
 // ! RACCOLTA DATI
 
 //* chiedo all'utente il numero di chlometri che vuole percorrrere e lo trasformo in numero
-const numberKm = parseInt(
+let numberKm = parseInt(
   prompt("Inserisci il numero di chilometri che devi percorrere")
 );
-console.log(numberKm);
+console.log(`Km da percorrere ${numberKm}`);
 
+//* prezzo €/Km
 const kmCost = 0.21;
-console.log(kmCost);
+console.log(`Il prezzo al Km è € ${kmCost}`);
+
+//* chiedo l'eta del passeggero
+const passengerAge = parseInt(prompt("Inserisci l'eta del passeggero"));
+console.log(`L'eta del passeggero è ${passengerAge} anni`);
 
 //* controllo che sia valido
 const isNumberKmValid = !isNaN(numberKm) && numberKm > 0;
 
-if (!isNumberKmValid) {
-  alert("Il valore inserito non è valido");
-}
-
-//* chiedo l'eta del passeggero
-const passengerAge = parseInt(prompt("Inserisci l'eta del passeggero"));
-console.log(passengerAge);
-
 //* controllo che sia valido
 const isPassengerAgeValid = !isNaN(passengerAge) && passengerAge > 0;
 
-if (!isPassengerAgeValid) {
-  alert("Il valore inserito non è valido");
-}
-
+// # PROCEDURA
 // ! ELABORAZIONE
+if (isNumberKmValid && isPassengerAgeValid) {
+  //* calcolo prezzo biglietto (0.21 € al km)
+  const priceTicket = numberKm * kmCost;
+  console.log(`Il prezzo del biglietto è € ${priceTicket.toFixed(2)}`);
 
-//* calcolo prezzo biglietto (0.21 € al km)
-const priceTicket = numberKm * kmCost;
-console.log(priceTicket);
+  //* sconto del 20% da applicare per gli under 18
+  if (passengerAge < 18) {
+    const discountForUnder18 = 20;
+    const priceUnder18 = (priceTicket - discountForUnder18) % 100;
+    alert("Hai meno di 18 anni quindi viene applicato uno sconto del 20%");
+    console.log(`Lo sconto è di € ${priceUnder18.toFixed(2)}`);
+  }
 
-//* sconto del 20% da applicare per gli under 18
-if (passengerAge < 18) {
-  const discountForUnder18 = 20;
-  const priceUnder18 = priceTicket - discountForUnder18;
-  console.log(priceUnder18.toFixed(2));
-  alert("Hai meno di 18 anni quindi il biglietto è scontato del 20%");
-}
-
-//* sconto del 40% da applicare per gli over 65
-if (passengerAge > 65) {
-  const discountForOver65 = 40;
-  const priceOver65 = priceTicket - discountForOver65;
-  console.log(priceOver65.toFixed(2));
-  alert("Hai più di 65 anni quindi il biglietto è scontato del 40%");
+  //* sconto del 40% da applicare per gli over 65
+  if (passengerAge > 65) {
+    const discountForOver65 = 40;
+    const priceOver65 = (priceTicket - discountForOver65) % 100;
+    alert("Hai più di 65 anni quindi viene applicato uno sconto del 40%");
+    console.log(`Lo sconto è di € ${priceOver65.toFixed(2)}`);
+  }
+} else {
+  alert(`ERRORE :
+    I Km inseriti non sono validi.
+    L'età inserita non è valida.
+    `);
 }
 
 // ! OUTPUT
 
 console.log(`
 numberKm: ${numberKm}
-kmCost:${kmCost}
-passengerAge:${passengerAge}
-priceTicket:${priceTicket}
+kmCost: ${kmCost}
+passengerAge: ${passengerAge}
 `);
